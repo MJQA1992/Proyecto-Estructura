b9c3c7910a47b7ba8f3a72ef7ee663ecc7edb967
Manuel Jose Quevedo Araujo <mjquevedo.22@est.ucab.edu.ve>
2025-10-18 11:02:58 -0400

    Validaciones / Limpieza
    Puliendo outputs, y validacioens en general

--- projecto.cpp
+++ projecto.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <cctype>
 
 using namespace std;
 
@@ -24,62 +25,199 @@ struct Lista {
 
     };
 
-//Agregar personajes parametro direccion del tope de la lista dada
+//Funcion para revisar si es constituida por espacios en blancos
+bool validacionEspacios(const string& str) {
+
+    if (str.empty()) return false;
+
+    for (char c : str) //Recorre caracter a caracter en la cadena para verifiicar si constituido de espacios
+
+        if (!isspace(static_cast<unsigned char>(c)))
+
+            return false;
+    return true;
+}
+
+//Funcion para revisar si es constituida de numeros
+bool validacionNumeros(const string& str) {
+
+    if (str.empty()) return false;
+
+    for (char c : str) //REcorre caracter a caracter en la cadena para verificar si esta constituido de numeros
+
+        if (!isdigit(static_cast<unsigned char>(c)))
+
+            return false;
+    return true;
+}
+
+//Agregar personajes por parametro direccion del tope de la lista dada
 void agregarPersonaje (Personaje*& cabeza ) {
 
             Personaje *nuevoPtr = new Personaje();
+            nuevoPtr->siguiente = nullptr;
 
-            cout << "Introduzca el ID del personaje: " << endl;
-            cin >> nuevoPtr->identificador;
-            cin.clear(); // Limpieza buffer
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Prevenir errores de input
-            cout << "El ID introducido es: " << nuevoPtr->identificador << endl;
+            // Validacion ID
+            do {
 
-            cout << "Introduzca la CLASE del personaje: " << endl;
-            getline(cin, nuevoPtr->clase); // Agarra toda la linea en vez de una sola cadena de caracteres
-            cin.clear();
-            cout << "La CLASE introducida es: " <<nuevoPtr->clase << endl;
+                cout << "Introduzca el ID del personaje: " << endl;
+                cin >> nuevoPtr->identificador;
+
+                if (cin.fail()) { //cin.fail verifica si es del tipo int o  no
+
+                    cin.clear(); // Limpieza buffer
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Prevenir errores de input
+                    cout <<"ERROR. Entrada de dato no valida." << endl;
+                }
+                else{
+
+                     cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Prevenir errores de input
+                     cout << "El ID introducido es: " << nuevoPtr->identificador << endl;
+                     break;
+                }
+            }while( true);
+
+            // Validacion CLASE
+            do {
+
+                cout << "Introduzca la CLASE del personaje: " << endl;
+                getline(cin, nuevoPtr->clase);
+
+                if (nuevoPtr->clase.empty()) {
+
+                    cout << "ERROR: La CLASE no puede estar vacia.\n";
+                }
+                else if (validacionEspacios(nuevoPtr->clase)) {
+
+                    cout << "ERROR. La CLASE no puede contener solo espacios.\n";
+                }
+                else if (validacionNumeros(nuevoPtr->clase)) {
+
+                    cout << "ERROR. La CLASE no puede ser solo de numeros.\n";
+                }
+                else {
+
+                    cout << "La CLASE introducida es: " << nuevoPtr->clase << endl;
+                    break;
+                }
+            } while (true);
+
+        // Validacion FACCION
+            do {
 
             cout << "Introduzca la FACCION del personaje: " << endl;
             getline(cin, nuevoPtr->faccion);
-            cin.clear();;
-            cout << "El  FACCION introducida es: " << nuevoPtr->faccion << endl;
+
+            if (nuevoPtr->faccion.empty()) {
+
+                cout << "ERROR. La FACCION no puede estar vacia.\n";
+            }
+            else if (validacionEspacios(nuevoPtr->faccion)) {
+
+                cout << "ERROR. La FACCION no puede contener solo espacios.\n";
+            }
+            else if (validacionNumeros(nuevoPtr->faccion)) {
+
+                cout << "ERROR. La FACCION no puede ser solo de numeros.\n";
+            }
+            else {
+
+                cout << "La FACCION introducida es: " << nuevoPtr->faccion << endl;
+                break;
+            }
+        } while (true);
+
+        // Validacion NOMBRE
+        do {
 
             cout << "Introduzca el NOMBRE del personaje: " << endl;
             getline(cin, nuevoPtr->nombre);
-            cin.clear();
-            cout << "El NOMBRE introducido es: " << nuevoPtr->nombre << endl;
+
+            if (nuevoPtr->nombre.empty()) {
+
+                cout << "ERROR. El NOMBRE no puede estar vacio.\n";
+            }
+            else if (validacionEspacios(nuevoPtr->nombre)) {
+
+                cout << "ERROR. El NOMBRE no puede contener solo espacios.\n";
+            }
+            else if (validacionNumeros(nuevoPtr->nombre)) {
+
+                cout << "ERROR. El NOMBRE no puede ser solo de numeros.\n";
+            }
+            else {
+
+                cout << "El NOMBRE introducido es: " << nuevoPtr->nombre << endl;
+                break;
+            }
+        } while (true);
+
+        // VALIDACIÃ“N ESTADO
+        do {
 
             cout << "Introduzca el ESTADO del personaje: " << endl;
             getline(cin, nuevoPtr->estado);
-            cin.clear();
-            cout << "El ESTADO introducido es: " <<nuevoPtr->estado << endl;
 
-            cout << "Introduzca los PUNTOS DE VIDA del personaje: " << endl;
-            cin >> nuevoPtr->vida;
-            cin.clear();
-            cout << "Los PUNTOS DE VIDA  introducidos es: " << nuevoPtr->vida << endl;
+            if (nuevoPtr->estado.empty()) {
 
+                cout << "ERROR. El ESTADO no puede estar vacio.\n";
+            }
+            else if (validacionEspacios(nuevoPtr->estado)) {
+
+                cout << "ERROR. El ESTADO no puede contener solo espacios.\n";
+            }
+            else if (validacionNumeros(nuevoPtr->estado)) {
+
+                cout << "ERROR. El ESTADO no puede ser solo de numeros.\n";
+            }
+            else {
+
+                cout << "El ESTADO introducido es: " << nuevoPtr->estado << endl;
+                break;
+            }
+        } while (true);
+
+             //Valdiacion VIDA
+             do {
+
+                cout << "Introduzca los PUNTOS DE VIDA del personaje: " << endl;
+                cin >> nuevoPtr->vida;
+
+                if (cin.fail()) { //cin.fail verifica si es del tipo int o  no
+
+                    cin.clear(); // Limpieza buffer
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Prevenir errores de input
+                    cout <<"ERROR. Entrada de PUNTOS DE VIDA no valido." << endl;
+                }
+            else{
+
+                     cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Prevenir errores de input
+                     cout << "Los PUNTOS DE VIDA  introducidos son: " << nuevoPtr->vida << endl;
+                     break;
+                }
+            }while( true);
+
+            //Enlace con la lista
             nuevoPtr->siguiente = cabeza; // Asignando nulo al final de la lista
             cabeza = nuevoPtr; // Asignando la nueva cabeza como el nuevo Personaje
     };
 
 //FUNCION PARA MOSTRAR LISTA
-void mostrarLista(Personaje* cabeza){
+void mostrarLista(Personaje*& cabeza){
 
-        if (cabeza == nullptr){
+        Personaje* temp = cabeza;
+        if (temp == nullptr){
 
             cout << "Lista Vacia." << endl;
         }
         else {
-            while (cabeza != nullptr ){
+            while (temp != nullptr ){
 
-            cout << "ID: " << cabeza->identificador << " CLASE: " << cabeza->clase <<  " FACCION: "<<  cabeza->faccion << " NOMBRE: " << cabeza->nombre << " ESTADO: " << cabeza->estado << " PUNTOS DE VIDA: " << cabeza->vida << endl;
-            cabeza = cabeza->siguiente;
+            cout << "ID: " << temp->identificador << "\nCLASE: " << temp->clase <<  "\nFACCION: "<<  temp->faccion << "\nNOMBRE: " << temp->nombre << "\nESTADO: " << temp->estado << "\nPUNTOS DE VIDA: " << temp->vida << "\n//*************************************//\n" << endl;
+            temp = temp->siguiente;
         };
             cout << "Lista mostrada completamente." << endl;
         }
-
         };
 
 // FUNCION PARA BORRAR TODA LA LISTA.
@@ -94,33 +232,66 @@ void borrarLista(Personaje*& cabeza) {
         cout << "Borrando al personaje: " << temp->nombre << " (ID: " << temp->identificador << ")." << endl ;
         delete temp;
     }
+    cout << "Lista Vacia." << endl;
     cabeza = nullptr;
 }
 
 //FUNCION GENERICA DE SELECCION DE PERSONAJES
-int seleccion(Personaje* cabeza) {
+int seleccion(Personaje*& cabeza) {
+
+    Personaje* temp = cabeza;
+    if (temp == nullptr){
+        cout << "No hay personajes disponibles." << endl;
+    }
 
     int id = 1;
     int seleccion;
-    while (cabeza != nullptr) {
-        cout << id << ". " << cabeza->nombre << " (ID: " << cabeza->identificador << ")\n";
-        cabeza = cabeza->siguiente;
+
+    while (temp != nullptr) {
+
+        cout << id << ". " << temp->nombre << " (ID: " << temp->identificador << ")\n";
+        temp = temp->siguiente;
         id++;
     }
-     cout << "Seleccione un personaje :\n";
-     cin >> seleccion;
+
+    //Validacion Seleccion
+    do{
+
+         cout << "Seleccione un personaje :\n";
+         cin >> seleccion;
+
+        if (cin.fail()) {
+
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "ERROR. Debe ingresar un numero valido.\n";
+        }
+        else if (seleccion < 1 || seleccion > id) {
+
+            cout << "ERROR. Seleccion fuera de rango.\n";
+        }
+
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
+            }
+        } while (true);
 
      return seleccion;
 }
 
 //FUNCION PARA BORRAR PERSONAJE POR FUNCION SELECCION
-void personajeEliminar(Personaje* cabeza, int seleccion) {
+void personajeEliminar(Personaje*& cabeza, int seleccion) {
 
     Personaje* actualPtr = cabeza;
-    Personaje* previoPtr;
-    Personaje* tempPtr ;
+    Personaje* previoPtr = nullptr;
+    Personaje* tempPtr = nullptr ;
     int id = 1;
 
+    if (actualPtr == nullptr) {
+        cout << "La lista estÃ¡ vacÃ­a, no hay personajes para eliminar." << endl;
+        return;
+    }
     while (actualPtr != nullptr && id < seleccion) {
 
         previoPtr = actualPtr;
@@ -147,13 +318,13 @@ void personajeEliminar(Personaje* cabeza, int seleccion) {
     }
 
     //FUNCION PARA MODIFICAR PERSONAJE EN SUS DISTINTOS CAMPOS.
-void personajeModificar(Personaje* cabeza, int seleccion) {
+void personajeModificar(Personaje*& cabeza, int seleccion) {
 
         Personaje* actualPtr = cabeza;
         int id = 1;
         int campo;
 
-    while (actualPtr != nullptr && id < seleccion) {
+    while (actualPtr != nullptr && id <=  seleccion) {
 
         actualPtr = actualPtr->siguiente;
         id++;
@@ -165,7 +336,7 @@ void personajeModificar(Personaje* cabeza, int seleccion) {
          cout <<"Campos a modificar: " << endl;
          cout << "1. ID del personaje" << endl;
         cout << "2. Clase del personaje" << endl;
-        cout << "3. Bando del personaje " << endl;
+        cout << "3. Faccion del personaje " << endl;
         cout << "4. Nombre del personaje" << endl;
         cout << "5. Estado del personaje "<< endl;
         cout << "6. Puntos de vida del personaje "<< endl;
@@ -186,19 +357,16 @@ void personajeModificar(Personaje* cabeza, int seleccion) {
             cin.clear();
             break;
         case 3:
-            cout << "Indique el nuevo bando: " << endl;
+            cout << "Indique la nueva faccion: " << endl;
             getline(cin, actualPtr->faccion);
-            cin.clear();
             break;
         case 4:
             cout << "Indique el nuevo nombre: " << endl;
             getline(cin, actualPtr->nombre);
-            cin.clear();
             break;
         case 5:
             cout << "Indique el nuevo estado: " << endl;
             getline(cin, actualPtr->estado);
-            cin.clear();
             break;
         case 6:
             cout << "Indique los nuevos puntos de vida:  " << endl;
@@ -206,12 +374,12 @@ void personajeModificar(Personaje* cabeza, int seleccion) {
             cin.clear();
             break;
         default:
-            cout << "Opci¨®n inv¨¢lida." << endl;
+            cout << "OpciÃ³n invÃ¡lida." << endl;
             break;
         }
     }
 
-void mostrarMenu(Personaje* lista){
+void mostrarMenu(Personaje*& lista){
     int campo = -1;
     do {
 
@@ -223,6 +391,8 @@ void mostrarMenu(Personaje* lista){
         cout << "5. Mostrar lista" << endl;
         cout << "6. Salir "<< endl;
         cin >> campo;
+        cin.clear(); // limpiando bufer
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
          switch (campo) {
             case 1:
@@ -241,11 +411,12 @@ void mostrarMenu(Personaje* lista){
                  mostrarLista(lista);
                 break;
              case 6:
+                  borrarLista(lista);
                  cout << "Programa TERMINADO." << endl;
                  campo = 6;
                 break;
             default:
-                cout << "Opci¨®n inv¨¢lida." << endl;
+                cout << "Opcion invalida." << endl;
                 break;
             }
         } while( campo !=6 );
